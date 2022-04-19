4ae39875e43e8bc6653593b3170ba88d8adaff6d
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-19 09:40:34 -0500

    Add common middleware for APIs

---
+++ src/middleware/index.ts
@@ -0,0 +1,5 @@
+export * from './caseInsensitiveRouting'
+export * from './customPoweredByHeader'
+export * from './jsonBodyParser'
+export * from './metrics'
+export * from './standardResponses'
---
+++ src/middleware/standardResponses/standardErrors.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express'
+
+import { ExpressError } from '../../ExpressError'
+import { getResponseMetadata } from './getResponseMetadata'
+import { ApiError, ApiErrorResponse } from './jsonApi'
+
+export const standardErrors = (err: ExpressError, req: Request, res: Response, next: NextFunction) => {
+  if (!err) {
+    next(err)
+    return
+  }
+  console.error(err.message)
+  if (!err.statusCode) err.statusCode = 500
+
+  const error: ApiError = {
+    detail: err.message,
+    status: `${err.statusCode}`,
+    title: err.name,
+  }
+  const body: ApiErrorResponse = { errors: [error] }
+  const meta = getResponseMetadata(res)
+  if (meta) {
+    body.meta = meta
+  }
+
+  res.status(err.statusCode).json(body)
+
+  next(err)
+}
