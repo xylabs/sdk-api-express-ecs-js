63fa6ca255759e38054541209c78147da025adf1
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-11 17:52:24 -0500

    Tests for getEnvFromAws

--- src/getEnvFromAws/getEnvFromAws.spec.ts
+++ src/getEnvFromAws/getEnvFromAws.spec.ts
@@ -1,11 +1,10 @@
 import { getEnvFromAws } from './getEnvFromAws'
-test('all', async () => {
-  expect.assertions(1)
-  try {
-    return await getEnvFromAws('does-not-exist')
-  } catch (ex) {
-    const error = ex as Error
-    console.log(`error: ${ex}`)
-    expect(error.name).toEqual('ResourceNotFoundException')
-  }
+
+const nonExistentSecret = 'does-not-exist'
+const errorMessage = "Secrets Manager can't find the specified secret."
+
+describe('getEnvFromAws', () => {
+  test('gets the ENV from AWS', async () => {
+    await expect(async () => await getEnvFromAws(nonExistentSecret)).rejects.toThrow(errorMessage)
+  })
 })
--- src/getEnvFromAws/getEnvFromAws.ts
+++ src/getEnvFromAws/getEnvFromAws.ts
@@ -1,18 +1,13 @@
 import { SecretsManager } from '@aws-sdk/client-secrets-manager'
 import NodeCache from 'node-cache'
 
+const region = 'us-east-1'
 const envCache = new NodeCache({ stdTTL: 3600 * 24 })
 
 export const getEnvFromAws = async (secretId: string) => {
   const cacheResult = envCache.get<Record<string, string>>(secretId)
-
   if (!cacheResult) {
-    const region = 'us-east-1'
-
-    const client = new SecretsManager({
-      region: region,
-    })
-
+    const client = new SecretsManager({ region })
     const awsResult = await client.getSecretValue({ SecretId: secretId })
     console.log(`ENV read from AWS Success [${awsResult?.Name}, ${!!awsResult?.SecretString}, ${!!awsResult?.SecretBinary}]`)
     if (awsResult?.SecretString) {
