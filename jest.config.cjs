51490f19475c4f22bb78f0be05c01864337374f6
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-11 17:19:45 -0500

    Bump deps

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,34 @@
-module.exports = {
-  preset: 'ts-jest'
+const generateJestConfig = ({ esModules }) => {
+  const esModuleslist = Array.isArray(esModules) ? esModules.join('|') : esModules
+  return {
+    coveragePathIgnorePatterns: ['<rootDir>/src/Profiler.ts'],
+    coverageThreshold: {
+      global: {
+        branches: 50,
+        functions: 70,
+        lines: 70,
+        statements: 70,
+      },
+    },
+    globals: {
+      'ts-jest': {
+        tsconfig: 'tsconfig.test.json',
+      },
+    },
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    preset: 'ts-jest/presets/default-esm',
+    setupFiles: [],
+    setupFilesAfterEnv: ['jest-sorted', 'jest-extended/all'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    testTimeout: 15000,
+    transform: {
+      [`(${esModuleslist}).+\\.js$`]: 'babel-jest',
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    transformIgnorePatterns: [`./node_modules/(?!${esModuleslist})`],
+  }
 }
+
+module.exports = generateJestConfig({ esModules: ['is-ip', 'ip-regex'] })
