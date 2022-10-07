cb44a14f67946fd2788c0155059c4b39a1b8a693
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-07 14:44:56 -0500

    Add Rollbar to default transports

--- src/Logger/getLogger.ts
+++ src/Logger/getLogger.ts
@@ -5,6 +5,7 @@ import { logFormatLocalDev, logFormatStructured } from './LogFormats'
 import { Logger } from './Logger'
 import { LoggerVerbosity } from './LoggerVerbosity'
 import { toWinstonVerbosity } from './toWinstonVerbosity'
+import { canGetDefaultRollbarTransport, getDefaultRollbarTransport } from './Transports'
 import { WinstonVerbosity } from './WinstonVerbosity'
 import { WrappedWinstonLogger } from './WrappedWinstonLogger'
 
@@ -15,6 +16,14 @@ const { Console } = winstonTransports
 const format = process.env.NODE_ENV === 'development' ? logFormatLocalDev : logFormatStructured
 const consoleTransport = new Console()
 const transports: TransportStream[] = [consoleTransport]
+if (canGetDefaultRollbarTransport()) {
+  try {
+    const rollbarTransport = getDefaultRollbarTransport()
+    transports.push(rollbarTransport)
+  } catch (error) {
+    // TODO: How to log errors with no logger?
+  }
+}
 
 const loggers: Record<WinstonVerbosity, Logger | undefined> = {
   debug: undefined,
