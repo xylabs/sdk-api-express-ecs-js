b0484eefa5b12391e16b77def40b235810ff15af
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-07 14:39:19 -0500

    Assert requisite ENV VARs exist before creation

--- src/Logger/Transports/Rollbar/getDefaultRollbarTransport.ts
+++ src/Logger/Transports/Rollbar/getDefaultRollbarTransport.ts
@@ -1,9 +1,10 @@
+import { assertEx } from '@xylabs/assert'
 import Rollbar from 'rollbar'
 
 import { RollbarTransport } from './RollbarTransport'
 
 export const getDefaultRollbarTransport = (): RollbarTransport => {
-  const accessToken = process.env.ROLLBAR_ACCESS_TOKEN
+  const accessToken = assertEx(process.env.ROLLBAR_ACCESS_TOKEN, 'Missing ROLLBAR_ACCESS_TOKEN ENV VAR')
   const rollbar: Rollbar = new Rollbar({ accessToken })
   return new RollbarTransport({}, rollbar)
 }
