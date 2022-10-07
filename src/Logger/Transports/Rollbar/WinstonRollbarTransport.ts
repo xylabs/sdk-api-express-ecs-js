402bc7a7bc00f921fc05fbb02db59b5dc4187468
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-07 12:37:43 -0500

    Add Winston Rollbar Transport

---
+++ src/Logger/Transports/Rollbar/WinstonRollbarTransport.ts
@@ -0,0 +1,15 @@
+import Rollbar from 'rollbar'
+import Transport, { TransportStreamOptions } from 'winston-transport'
+
+export class WinstonRollbarTransport extends Transport {
+  constructor(opts: TransportStreamOptions, protected readonly rollbar?: Rollbar) {
+    super(opts)
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  log(info: any, next: () => void) {
+    this.rollbar?.error(info)
+    this.emit('logged', info)
+    next()
+  }
+}
