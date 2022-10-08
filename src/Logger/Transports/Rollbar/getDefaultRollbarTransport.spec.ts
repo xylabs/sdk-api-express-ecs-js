9826b05e83dc7c6b41ce0542bb0c8195be855052
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-07 21:34:22 -0500

    test coverage for transport factory method

---
+++ src/Logger/Transports/Rollbar/getDefaultRollbarTransport.spec.ts
@@ -0,0 +1,10 @@
+import { getDefaultRollbarTransport } from './getDefaultRollbarTransport'
+
+describe('getDefaultRollbarTransport', () => {
+  it('returns the transport', () => {
+    const env = { ROLLBAR_ACCESS_TOKEN: 'something' }
+    const transport = getDefaultRollbarTransport(env)
+    expect(transport).toBeObject()
+    expect(transport.log).toBeFunction()
+  })
+})
