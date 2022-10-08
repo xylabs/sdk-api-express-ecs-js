daabe6ca9b8f3abad8365f8da1bdadb7d66a64aa
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-07 21:23:38 -0500

    Inject environment for testability

--- src/Logger/Transports/Rollbar/getDefaultRollbarTransport.ts
+++ src/Logger/Transports/Rollbar/getDefaultRollbarTransport.ts
@@ -3,8 +3,8 @@ import Rollbar from 'rollbar'
 
 import { RollbarTransport } from './RollbarTransport'
 
-export const getDefaultRollbarTransport = (): RollbarTransport => {
-  const accessToken = assertEx(process.env.ROLLBAR_ACCESS_TOKEN, 'Missing ROLLBAR_ACCESS_TOKEN ENV VAR')
+export const getDefaultRollbarTransport = (env: { [key: string]: string | undefined }): RollbarTransport => {
+  const accessToken = assertEx(env.ROLLBAR_ACCESS_TOKEN, 'Missing ROLLBAR_ACCESS_TOKEN ENV VAR')
   const rollbar: Rollbar = new Rollbar({ accessToken })
   return new RollbarTransport({}, rollbar)
 }
