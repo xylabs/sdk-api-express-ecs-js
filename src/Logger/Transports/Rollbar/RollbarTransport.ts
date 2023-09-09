8c2f25f14200387e50bfcd5918ad4b8386fddf6c
Arie Trouw <arie.trouw@xylabs.com>
2023-09-09 11:46:35 -0700

    updo & tsup

--- src/Logger/Transports/Rollbar/RollbarTransport.ts
+++ src/Logger/Transports/Rollbar/RollbarTransport.ts
@@ -4,12 +4,15 @@ import Transport, { TransportStreamOptions } from 'winston-transport'
 import { logFormatRollbar } from '../../LogFormats'
 
 export class RollbarTransport extends Transport {
-  constructor(opts: TransportStreamOptions, protected readonly rollbar?: Rollbar) {
+  constructor(
+    opts: TransportStreamOptions,
+    protected readonly rollbar?: Rollbar,
+  ) {
     super({ ...opts, format: logFormatRollbar, level: 'error' })
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  log(info: { message?: string }, next: () => void) {
+  override log(info: { message?: string }, next: () => void) {
     this.rollbar?.error(info?.message)
     this.emit('logged', info?.message)
     next()
