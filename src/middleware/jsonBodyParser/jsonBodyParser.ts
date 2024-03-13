b4b3523447f8e519fa41df4875bd23ca3820a431
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-03-13 07:37:42 -0500

    Add JSON Body Parser helper

--- src/middleware/jsonBodyParser/jsonBodyParser.ts
+++ src/middleware/jsonBodyParser/jsonBodyParser.ts
@@ -1,4 +1,5 @@
-import bodyParser from 'body-parser'
+import bodyParser, { OptionsJson } from 'body-parser'
+import { NextHandleFunction } from 'connect'
 import { NextFunction, Request, Response } from 'express'
 
 const bodyParserInstance = bodyParser.json({ type: ['application/json', 'text/json'] })
@@ -11,4 +12,27 @@ export const jsonBodyParser = (req: Request, res: Response, next: NextFunction) 
     const error = ex as Error
     console.log(`bodyParser failed [${error.name}]: ${error.message}`)
   }
+}
+
+export const DefaultJsonBodyParserOptions: OptionsJson = { type: ['application/json', 'text/json'] }
+
+/**
+ * Get a JSON Body Parser connect middleware handler
+ * @param options The options for the JSON Body Parser
+ * @returns A middleware function that parses JSON bodies
+ */
+export const getJsonBodyParser = (options: OptionsJson = DefaultJsonBodyParserOptions): NextHandleFunction => {
+  // Create closed instance of bodyParser to prevent instantiation of new instance on every request
+  const parser = bodyParser.json(options)
+
+  // If we do not trap this error, then it dumps too much to log, usually happens if request aborted
+  const ret: NextHandleFunction = (req, res, next) => {
+    try {
+      parser(req, res, next)
+    } catch (ex) {
+      const error = ex as Error
+      console.log(`bodyParser failed [${error.name}]: ${error.message}`)
+    }
+  }
+  return ret
 }
